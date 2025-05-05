cea7fd80e6c386c2fef74b913b7b08cbf50810b5
maddycha <maddycha@gmail.com>
2025-05-04 22:15:46 -0400

    add new button

--- rhythm-ring/js/script.js
+++ rhythm-ring/js/script.js
@@ -1,12 +1,24 @@
-function openAbout() {
-  document.getElementById("about").style.display = "block";
-  document.getElementById("members").style.display = "none";
-  document.getElementById("join").style.display = "none";
+function openPage(x) {
+  document.getElementById("aboutpage").style.display = "none";
+  document.getElementById("joinpage").style.display = "none";
+  document.getElementById("widgetpage").style.display = "none";
+  document.getElementById("memberspage").style.display = "none";
+  document.getElementById(x.id).style.display = "block";
+  if(x.id == "aboutpage"){
+    console.log("about");
+  }
+  // else if(x.id == "joinpage"){
+  //   console.log("members");
+  // } else if(x.id == "widgetpage"){
+  //   console.log("members");
+  // } else if(x.id == "memberspage"){
+  //   console.log("members");
+  // }
 }
 
 function populateMembers() {
   for(var i = 0; i < members.length; i++){
-    document.getElementById("members-table").innerHTML+= "<div class='member'><img src='"+members[i].img+"'><p>"+members[i].name+"</p><a href='"+members[i].url+"' target='_blank'><p>"+members[i].url+"</p></a></div>";
+    // document.getElementById("members-table").innerHTML+= "<div class='member'><img src='"+members[i].img+"'><p>"+members[i].name+"</p><a href='"+members[i].url+"' target='_blank'><p>"+members[i].url+"</p></a></div>";
 ;
   }
   console.log(members.name);
